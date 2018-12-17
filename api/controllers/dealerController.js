871fc095b1a5db773b16708f1b059426ad85be16
agileCoder <smfaisal1648@gmail.com>
2018-12-18 00:43:24 +0600

    sweet

--- api/controllers/dealerController.js
+++ api/controllers/dealerController.js
@@ -4,7 +4,10 @@ exports.dealCards = function(req, res) {
   if (global.dealtCards === undefined) {
       global.dealtCards = [];
   }
-  //if (global.dealtCards.length = 52) return resetDealer(req, res);
+  if (global.dealtCards.length === 52) {
+      res.json({msg: "game ended"});
+      return;
+  }
 
   var availableCards = [...Array(52).keys()].filter((x) => { return !global.dealtCards.includes(x); });
 
@@ -16,9 +19,31 @@ exports.dealCards = function(req, res) {
 
 exports.resetDealer = function(req, res) {
     global.dealtCards = [];
-    res.json({msg: "game reset"})
+    res.json({msg: "game reset"});
 };
 
 exports.registerMouseWheel = function(event) {
-    
+    if (this.prev === undefined) {
+        this.uptime = new Date();
+        this.downtime = new Date();
+        this.count = 0;
+    }
+    else {
+        var now = new Date();
+        if (event.rotation !== this.prev) {
+            if (event.rotation === 1) this.uptime = new Date();
+            else this.downtime = new Date();
+        }
+        var timediff = Math.abs(this.uptime.getTime() - this.downtime.getTime());
+        if (timediff < 300 && timediff > 0) {
+            if (this.count < 7) this.count++;
+            else {
+                this.count = 0;
+                this.uptime = this.downtime = now;
+                global.dealtCards = [];
+                console.log("Dealer reset ");
+            }
+        }
+    }
+    this.prev = event.rotation;
 };
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   ioHook = require('ioHook'),
+  dealer = require('./api/controllers/dealerController'),
   port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +14,7 @@ routes(app);
 app.listen(port);
 
 ioHook.on('mousewheel', event => {
-  registerMouseWheel(event);
+  dealer.registerMouseWheel(event);
 });
 ioHook.start();
 
