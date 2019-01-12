fd720cb14b64202318884f17f39fb91d73754623
Frank-en-stein <smfaisal1648@gmail.com>
2019-01-12 14:10:54 +0600

    nonsense

--- api/controllers/dealerController.js
+++ api/controllers/dealerController.js
@@ -37,7 +37,7 @@ exports.registerMouseWheel = function(event) {
             else this.downtime = new Date();
         }
         var timediff = Math.abs(this.uptime.getTime() - this.downtime.getTime());
-        if (timediff < 300 && timediff > 0) {
+        if (timediff < 500 && timediff > 0) {
             if (this.count < 7) this.count++;
             else {
                 this.count = 0;
